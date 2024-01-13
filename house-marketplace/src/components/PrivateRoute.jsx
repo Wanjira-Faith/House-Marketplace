27fb06d19c7a118904c6edfc3956358439e2d233
Wanjira-Faith <wanjirafaith22@gmail.com>
2024-01-13 11:32:15 +0300

    create useAuthstatus hook

--- house-marketplace/src/components/PrivateRoute.jsx
+++ house-marketplace/src/components/PrivateRoute.jsx
@@ -1,8 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom'
-import React from 'react'
+import useAuthStatus from '../hooks/useAuthStatus'
 
-function PrivateRoute() {
-    const loggedIn = false
+const PrivateRoute = () => {
+    const {loggedIn, checkingStatus} = useAuthStatus()
+
+    if(checkingStatus) {
+        return <h3>Loading...</h3>
+    }
     
   return loggedIn ? <Outlet/> : <Navigate to='/sign-in'/>
 }
