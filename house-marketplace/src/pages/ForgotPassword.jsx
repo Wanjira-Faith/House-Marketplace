287d2483dde59b7e950d049bfd563222cc238be0
Wanjira-Faith <wanjirafaith22@gmail.com>
2024-01-14 16:58:02 +0300

    import necessary modules

--- house-marketplace/src/pages/ForgotPassword.jsx
+++ house-marketplace/src/pages/ForgotPassword.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+import {getAuth, sendPasswwordResetEmai} from'firebase/auth'
+import {toast} from 'react-toastify'
+import { ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 function ForgotPassword() {
   return (
     <h1>Forgot Password</h1>
