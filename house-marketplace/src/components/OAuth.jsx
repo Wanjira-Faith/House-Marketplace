e43d81a3df646c1872affce4af9531dde4f6883b
Wanjira-Faith <wanjirafaith22@gmail.com>
2024-01-25 16:24:24 +0300

    check for user in google OAuth and create one if user does not exist

--- house-marketplace/src/components/OAuth.jsx
+++ house-marketplace/src/components/OAuth.jsx
@@ -1,5 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom"
-import {getAuth, signInWithPop, GoogleAuthProvider} from 'firebase/auth'
+import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
 import {doc, setDoc, getDoc} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
@@ -13,11 +13,25 @@ function OAuth() {
     try {
       const auth = getAuth()
       const provider = new GoogleAuthProvider()
-      const result = await signInWithPop(auth, provider)
+      const result = await signInWithPopup(auth, provider)
       const user = result.user
-      
+
+      // Check for user
+      const docRef = doc(db,'users', user.uid)
+      const docSnap = await getDoc(docRef)
+
+      //If user does not exist, create user
+      if(!docSnap.exists()) {
+        await setDoc(doc(db, 'users', user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp:serverTimestamp()
+        })
+      }
+      navigate('/')
+
     } catch (error) {
-      
+      toast.error('Could not authorize with Google')
     }
 
   }
