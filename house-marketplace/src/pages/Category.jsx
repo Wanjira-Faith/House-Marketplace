09a69e528f69b03a25e92a4d1e113d70136ad0c6
Wanjira-Faith <wanjirafaith22@gmail.com>
2024-01-27 09:00:29 +0300

    import necessary modules

--- house-marketplace/src/pages/Category.jsx
+++ house-marketplace/src/pages/Category.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import {collection, getDocs, query, where, orderBy, linit, startAfter} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
+import Spinner from '../components/Spinner'
 
 function Category() {
   return (
